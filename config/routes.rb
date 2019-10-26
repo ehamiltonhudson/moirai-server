f090ffea406f434ce4b770a5ad587ded5bf11d88
Hamilton <ehamiltonhudson@gmail.com>
2019-10-26 02:07:36 -0400

    adds api/v1, updates routes, adds gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :api do
+    resources :v1 do
+
+  end
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
