3de75d23d578bbe73ca587290671b759e785fb0b
Hamilton <ehamiltonhudson@gmail.com>
2019-11-04 02:18:44 -0500

    updates seeds to include images for cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,6 +65,35 @@ end
   {role: 'Queen', suit: 'Wands', rank: 12},
   {role: 'King', suit: 'Wands', rank: 14},
 ]
+
+@marseilles_wands = {
+  1 => 'https://drive.google.com/file/d/1RDjHzkL8zTRkf-YeTHVI65gnRxF-ye2E/view?usp=sharing',
+  2 => 'https://drive.google.com/file/d/1uQPI0K5gHkmQxx0XbFMK0lpS6MUTE6HH/view?usp=sharing',
+  3 => 'https://drive.google.com/file/d/13vb80hKQPsFyQ_DDFg0fFpHGLwPjgVWF/view?usp=sharing',
+  4 => 'https://drive.google.com/file/d/14O2k5uwemtnIrW8hEVVWWDJePVRLB07p/view?usp=sharing',
+  5 => 'https://drive.google.com/file/d/1JBrRdt0LqvEgbVICCYl65a3KNeu5Rs5Z/view?usp=sharing',
+  6 => 'https://drive.google.com/file/d/1Rv4jYNO8z3kUmswu7HsOshiDSybI1692/view?usp=sharing',
+  7 => 'https://drive.google.com/file/d/1WBudLkIMhgxjhPRqFSJ_H31UGGEVRm0f/view?usp=sharing',
+  8 => 'https://drive.google.com/file/d/154xAXlTdn_JzypJMTjIuBFZjvg5z-m4I/view?usp=sharing',
+  9 => 'https://drive.google.com/file/d/1cVpCMImqcxPHf34wpUElWPMVAlnHqmB7/view?usp=sharing',
+  10 => 'https://drive.google.com/file/d/1He-1c_g9QVBXF5NBW6EXrY0IkxX9OceE/view?usp=sharing',
+  11 => 'https://drive.google.com/file/d/14amBFE5W4pzGPbdVHGIYCuxlIg8StYP3/view?usp=sharing',
+  12 => 'https://drive.google.com/file/d/1EoN2Vtd_zMDwsDwv5hHnUHkZaSHCtM9p/view?usp=sharing',
+  13 => 'https://drive.google.com/file/d/1zHXZTwuJQei1rr1_ltd_acU2c0Ngh3mu/view?usp=sharing',
+  14 => 'https://drive.google.com/file/d/1rsLts0-iTuWwo3Yg5qC6Mp6axZ1-MWNk/view?usp=sharing'
+}
+c.image = @marseilles_wands[k]
+
+Card.all.find_all do |c|
+  c.suit == "Wands"
+  @marseilles_wands.map do |k, v|
+    if k == c.rank
+      c.update_attributes(image: @marseilles_wands[k])
+      c.save
+    end
+  end
+end
+
 @wands.each do |wand|
   @minor_arcana_cards.push(wand)
 end
@@ -86,6 +115,34 @@ end
   {role: 'Queen', suit: 'Pentacles', rank: 13},
   {role: 'King', suit: 'Pentacles', rank: 14}
 ]
+
+@marseilles_pentacles = {
+  1 => 'https://drive.google.com/file/d/1r8-pAEdARu4aX7k-XsIdruELR3zr0SO_/view?usp=sharing',
+  2 => 'https://drive.google.com/file/d/1-PKyBYrT6PZLvNY85Fr-RY6rssKV1vOE/view?usp=sharing',
+  3 => 'https://drive.google.com/file/d/1MRJrTrTYlFjvL-2pQgNClm75AtNcj6Un/view?usp=sharing',
+  4 => 'https://drive.google.com/file/d/14Z6w3nslnKZ81sB8DeBtKIme4VwjEJLo/view?usp=sharing',
+  5 => 'https://drive.google.com/file/d/1BWNxixN34KlT-Mawz_20wEIzBhdz1lCc/view?usp=sharing',
+  6 => 'https://drive.google.com/file/d/1S4068dLRNyQ6ky4BOlCwuuXlU-cIQVH9/view?usp=sharing',
+  7 => 'https://drive.google.com/file/d/1McnDTX7IUPdq6rFX6f-rLItk6j4hyZJv/view?usp=sharing',
+  8 => 'https://drive.google.com/file/d/1j22o2bga0OOzU2Ngjr9h2EZ-z30KJ5-f/view?usp=sharing',
+  9 => 'https://drive.google.com/file/d/145VvYTXlLEH5JFou3u8SsLsziCekhJaO/view?usp=sharing',
+  10 => 'https://drive.google.com/file/d/1CBhlMQxSi-1ZM79qhrNydh5LnxuRVSSF/view?usp=sharing',
+  11 => 'https://drive.google.com/file/d/1MXKuFwGHJudQ-uniwZcf82KNpiMVK8Tu/view?usp=sharing',
+  12 => 'https://drive.google.com/file/d/1sgU13yXEMGZHCkL-7vtSE2o-ehWSinki/view?usp=sharing',
+  13 => 'https://drive.google.com/file/d/15t4i5M_OQK-yRw37oStpC5ZNhqtmehjQ/view?usp=sharing',
+  14 => 'https://drive.google.com/file/d/1RJe1qfhxA70NubnLKxnJbCzmbFlDj4DI/view?usp=sharing'
+}
+
+Card.all.find_all do |c|
+  c.suit == "Pentacles"
+  @marseilles_pentacles.map do |k, v|
+    if k == c.rank
+      c.image = @marseilles_pentacles[k]
+      c.save
+    end
+  end
+end
+
 @pentacles.each do |pentacle|
   @minor_arcana_cards.push(pentacle)
 end
@@ -107,6 +164,33 @@ end
   {role: 'Queen', suit: 'Swords', rank: 13},
   {role: 'King', suit: 'Swords', rank: 14}
 ]
+
+@marseilles_swords = {
+  1 => 'https://drive.google.com/file/d/1VDPmF8Aar9M-eG_iE_UUNodbmfqrwsOn/view?usp=sharing',
+  2 => 'https://drive.google.com/file/d/1QXE_aX0el6CnbThR7hFFmH-Mcv7J8MzD/view?usp=sharing',
+  3 => 'https://drive.google.com/file/d/1LUhC86oDGNpNgobIVGaMSunU1jR5hlUk/view?usp=sharing',
+  4 => 'https://drive.google.com/file/d/1g6up0eZi367g8gLLRTaPxODGINmwCpUX/view?usp=sharing',
+  5 => 'https://drive.google.com/file/d/1ugJ4yh6YRQKFZnNm2e0XGgTzkh4_Q7CV/view?usp=sharing',
+  6 => 'https://drive.google.com/file/d/1KzyDoiOu11CMbel6aH9Zf9mNXdzSn8Nq/view?usp=sharing',
+  7 => 'https://drive.google.com/file/d/1qaBqAHe836iVRB0w0B2bxJj6e6_BVVT8/view?usp=sharing',
+  8 => 'https://drive.google.com/file/d/1UK-4AaslzZ46sSZwQRIfrt8_RpSo4apv/view?usp=sharing',
+  9 => 'https://drive.google.com/file/d/1Ie3X8dltqwCGOQLgtxH2SLfcAgh8M4M5/view?usp=sharing',
+  10 => 'https://drive.google.com/file/d/1qc6cGUBxPder3gnGYQQMEdLrK4kXc33f/view?usp=sharing',
+  11 => 'https://drive.google.com/file/d/1ADBWecTQy7WJjOPTgUJZRL3e80n878Zc/view?usp=sharing',
+  12 => 'https://drive.google.com/file/d/1Ov_p4SRhNOZ5HR7Bq3ILGKmYn5SIsuKF/view?usp=sharing',
+  13 => 'https://drive.google.com/file/d/1PFGh_KEwFynAVqP4o9GKXa-PQ341YzMc/view?usp=sharing',
+  14 => 'https://drive.google.com/file/d/1e3pYavQSTkXoAV1KcsHjm-2s1JzDQd7a/view?usp=sharing'
+}
+
+Card.all.find_all do |c|
+  c.suit == "Swords"
+  @marseilles_swords.map do |k, v|
+    if k == c.rank
+      c.update_attributes(image: @marseilles_swords[k])
+    end
+  end
+end
+
 @swords.each do |sword|
   @minor_arcana_cards.push(sword)
 end
@@ -128,6 +212,33 @@ end
   {role: 'Queen', suit: 'Chalices', rank: 13},
   {role: 'King', suit: 'Chalices', rank: 14}
 ]
+
+@marseilles_chalices = {
+  1 => 'https://drive.google.com/file/d/1lbujnCCACNJVVblJiQPkLEzCbJ3yZqTM/view?usp=sharing',
+  2 => 'https://drive.google.com/file/d/17884CsMQtk_eeHXgbGu83G45INec8tLj/view?usp=sharing',
+  3 => 'https://drive.google.com/file/d/1yvzoQ6J4fnlUKxu238vDKDjHOfPQ23gb/view?usp=sharing',
+  4 => 'https://drive.google.com/file/d/1Uj0jF1zS8l0NztuAlPuJATO84i43YILY/view?usp=sharing',
+  5 => 'https://drive.google.com/file/d/1hZgIbo6oA3PcAfpLJlSP4NL2fSC3tHea/view?usp=sharing',
+  6 => 'https://drive.google.com/file/d/14XMQ5S06ZZOwL2JGSdxPQIOfgtlDnctl/view?usp=sharing',
+  7 => 'https://drive.google.com/file/d/1_Q5bI17uZYvdpkncJbgri-kdCwQo13q3/view?usp=sharing',
+  8 => 'https://drive.google.com/file/d/1vPYWLEdnAv2tTkPeDzUu4shJHgwWCa1K/view?usp=sharing',
+  9 => 'https://drive.google.com/file/d/1Fmt7EsrCYoPSSVnv4x3yUG33flj3y4QG/view?usp=sharing',
+  10 => 'https://drive.google.com/file/d/1QmSgpt0qBTWiHeW7Y9P9kB207Qui-mhB/view?usp=sharing',
+  11 => 'https://drive.google.com/file/d/1xU-YD90xW7BxkAMRMBjE3zwRgH2B0ovZ/view?usp=sharing',
+  12 => 'https://drive.google.com/file/d/1zOT7hAfXzDpHGqMkRrTnssvBIkBal_CI/view?usp=sharing',
+  13 => 'https://drive.google.com/file/d/1PVUwlWXKczs9baoQyqNRo4LDwCebk4AS/view?usp=sharing',
+  14 => 'https://drive.google.com/file/d/19DTXJ2Ql7bh7jEXAMuZbs3fFvuq6kEat/view?usp=sharing'
+}
+
+Card.all.find_all do |c|
+  c.suit == "Chalices"
+  @marseilles_chalices.map do |k, v|
+    if k == c.rank
+      c.image = @marseilles_chalices[k]
+    end
+  end
+end
+
 @chalices.each do |chalice|
   @minor_arcana_cards.push(chalice)
 end
