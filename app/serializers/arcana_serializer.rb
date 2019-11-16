9e290f08478fcaaf7782c8d3403c6674ab516443
Hamilton <ehamiltonhudson@gmail.com>
2019-11-15 19:12:06 -0500

    adds cards to arcana serializer

--- app/serializers/arcana_serializer.rb
+++ app/serializers/arcana_serializer.rb
@@ -1,4 +1,5 @@
 class ArcanaSerializer < ActiveModel::Serializer
   attributes :id, :key, :count, :deck_id
   belongs_to :deck, serializer: DeckSerializer
+  has_many :cards, serializer: CardSerializer
 end
